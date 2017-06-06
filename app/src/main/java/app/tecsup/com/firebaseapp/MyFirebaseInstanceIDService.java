7add36a2437a0510ff48688276c532ecae51cdd6
Jhojan Guillermo <jhojan.guillermo@tecsup.edu.pe>
2017-06-06 16:46:27 -0500

    Initial commit

---
+++ app/src/main/java/app/tecsup/com/firebaseapp/MyFirebaseInstanceIDService.java
@@ -0,0 +1,27 @@
+package app.tecsup.com.firebaseapp;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+/**
+ * Created by JShanksX13 on 6/06/2017.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = MyFirebaseInstanceIDService.class.getSimpleName();
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        FirebaseMessaging.getInstance().subscribeToTopic("ALL");
+    }
+
+}
+
